547cc7ffcbfb8a3deb875ae86c9859a2566b5479
lymyr <176290600+lymyr@users.noreply.github.com>
2026-09-20 19:35:13 +0800

    feat(back): added cors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import session from "express-session"
 import indexRouter from "./routes/indexRouter.js"
+import cors from "cors"
 
 process.loadEnvFile()
 
@@ -14,6 +15,7 @@ app.use(session({
     saveUninitialized: true
 }))
 app.use(express.json())
+app.use(cors())
 
 app.use("/v1", indexRouter)
 
