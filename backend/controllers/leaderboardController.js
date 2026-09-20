e21d4f6342045cae8bda658f3ac9ac5ce08e4c36
lymyr <176290600+lymyr@users.noreply.github.com>
2026-09-20 22:07:32 +0800

    fix(back): endTime relying on new Date()

--- backend/controllers/leaderboardController.js
+++ backend/controllers/leaderboardController.js
@@ -4,7 +4,7 @@ export const addLeaderboard = async (req, res) => {
     const user = await prisma.leaderboard.create({data: {
         name: req.body.username,
         startTime: req.decodedToken.startTime,
-        endTime: new Date(),
+        endTime: req.decodedToken.endTime,
         gameId: req.decodedToken.gameId
     }})
     
