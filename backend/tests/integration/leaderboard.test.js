ddb3174612552f4115a12d0f3c44562cfc77dfd6
lymyr <176290600+lymyr@users.noreply.github.com>
2026-09-21 21:19:48 +0800

    tests(back): updated token payload to have endTime

--- backend/tests/integration/leaderboard.test.js
+++ backend/tests/integration/leaderboard.test.js
@@ -35,7 +35,8 @@ describe("POST /:gameId/leaderboard", () => {
         const jwtVerifyMock = jest.spyOn(jwt, "verify").mockReturnValue({
             gameId: 1,
             persons: [],
-            startTime: new Date(2000, 1)
+            startTime: new Date(2000, 1),
+            endTime: new Date(2000, 2)
         })
         const res = await request(app)
             .post("/v1/1/leaderboard")
